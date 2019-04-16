6986ab40498ff95cb160c6406a79d72cb2a5f14e
Scott Goyette <scott.goyette@rubypythmore.com>
2019-04-16 17:06:36 -0400

    proposal api working

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,10 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['localhost']
+
 module FreelanceZoneProposal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
